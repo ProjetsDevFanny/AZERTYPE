54ebd184d1d3933b6b388325dfdc6ca8af5c68eb
Fanny Simon <contact.fanny.simon@gmail.com>
2025-03-18 12:03:37 +0100

    centralisation des erreurs avec throw/try-catch

--- scripts/script.js
+++ scripts/script.js
@@ -80,26 +80,8 @@ function lancerJeu() {
   //   event.preventDefault();
   form.addEventListener("submit", (event) => {
     event.preventDefault();
-
-    const baliseNom = document.getElementById("nom");
-    const nom = baliseNom.value.trim();
-    // const regexNom = new RegExp(); // Synthaxe si la regex est dynamique (avec plusieurs variables)
-
-    console.log(validerNom(nom));
-
-    const baliseEmail = document.getElementById("email");
-    const email = baliseEmail.value.trim();
-
-    console.log(validerEmail(email));
-
-    if (validerNom(nom) === true && validerEmail(email) === true) {
-      let scoreEmail = `${score} / ${i}`;
-      afficherEmail(nom, email, scoreEmail);
-    } else {
-      alert(
-        "Veuillez rentrez un nom et un email valide svp (au moins 2 caractères pour le nom)"
-      );
-    }
+    let scoreEmail = `${score} / ${i}`;
+    gererFormulaire(scoreEmail);
   });
 
   afficherResultat(score, i);
@@ -125,36 +107,35 @@ function afficherEmail(nom, email, score) {
  * @throws {Error}
  */
 
+// autre écriture :
 function validerNom(nom) {
-  const regexNom = /^[A-Za-zÀ-ÖØ-öø-ÿ0-9' -]{2,}$/;
-  return regexNom.test(nom);
+  if (nom.length < 2) {
+    throw new Error("Le nom est trop court."); // Nous lançons une exception (gèrer par un try/catch dans la fonction "gererFormulaire")
+  }
 }
 
-// autre écriture :
 // function validerNom(nom) {
-//   if (nom.length >= 2) {
-//     return true;
-//   }
-//   return false;
-// };
+//   const regexNom = /^[A-Za-zÀ-ÖØ-öø-ÿ0-9' -]{2,}$/;
+//   return regexNom.test(nom);
+// }
 
 /**
  * Cette fonction prend un email en paramètre et valide qu'il est au bon format.
  * @param {string} email
  * @throws {Error}
  */
-function validerEmail(email) {
-  const regexEmail = /^[\w_-]+@[\w-]+\.[a-z]{2,4}$/i;
-  return regexEmail.test(email);
-}
 
 // autre écriture :
+function validerEmail(email) {
+  let regexEmail = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\\.[a-z0-9._-]+");
+  if (!regexEmail.test(email)) {
+    throw new Error("L'email n'est pas valide."); // Nous lançons une exception (gèrer par un try/catch dans la fonction "gererFormulaire")
+  }
+}
+
 // function validerEmail(email) {
-//   let regerxEmail = new RegExp ("[a-z0-9._-]+@[a-z0-9._-]+\\.[a-z0-9._-]+");
-//   if(regerxEmail.text(email)) {
-//     return true
-//   }
-//   return false
+//   const regexEmail = /^[\w_-]+@[\w-]+\.[a-z]{2,4}$/i;
+//   return regexEmail.test(email);
 // }
 
 /**
@@ -164,11 +145,44 @@ function validerEmail(email) {
  * @param {string} message
  */
 
+function afficherMessageErreur(message) {
+  // On ne veut pas une création du span à chaque click du bouton, sinon, création à l'infini !
+  let spanErreurMessage = document.getElementById("erreurMessage");
+
+  //Si le span n'existe pas, on va le créer:
+  if (!spanErreurMessage) {
+    const popup = document.querySelector(".popup");
+    spanErreurMessage = document.createElement("span");
+    spanErreurMessage.id = "erreurMessage";
+    popup.append(spanErreurMessage); // append = "ajouter"
+  }
+  //Sinon, si le span existe, on lui met un message
+  spanErreurMessage.innerText = message;
+}
+
 /**
  * Cette fonction permet de récupérer les informations dans le formulaire
  * de la popup de partage et d'appeler l'affichage de l'email avec les bons paramètres.
  * @param {string} scoreEmail
  */
+
+function gererFormulaire(scoreEmail) {
+  try {
+    // gèrer les exceptions lancées avant
+    const baliseNom = document.getElementById("nom");
+    const nom = baliseNom.value.trim();
+    // const regexNom = new RegExp(); // Synthaxe si la regex est dynamique (avec plusieurs variables)
+    validerNom(nom);
+
+    const baliseEmail = document.getElementById("email");
+    const email = baliseEmail.value.trim();
+    validerEmail(email);
+    afficherMessageErreur("");
+    afficherEmail(nom, email, scoreEmail);
+  } catch (erreur) {
+    afficherMessageErreur(erreur.message);
+  }
+}
 
 let sujet = "";
 let message = "";
